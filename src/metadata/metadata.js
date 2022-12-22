d5df888e7e85a449f508b3edf283b1dc0a6142b5
Ravindra <patil.ravi035@gmail.com>
2022-12-22 13:25:26 +0530

    updated profile

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,68 +1,125 @@
 module.exports = {
-  name: 'William Shakespeare',
-  title: 'Poet, playwright and actor',
+  name: "Ravindra Patil",
+  title: "Senior Software Engineer",
   facts: {
-    'Residence': '<a href="https://www.google.com/maps/place/London,+UK/@51.5285582,-0.2416814,11z/data=!3m1!4b1!4m5!3m4!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5072178!4d-0.1275862"><i class="fa fa-home fact-icon"></i>London</a>',
-    'Wiki': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
-                  <i class="fa-brands fa-wikipedia-w fact-icon"></i>William_Shakespeare
+    Residence:
+      '<a href="https://www.google.com/maps/place/Bengaluru,+Karnataka/@12.9544595,77.3012657,10z/data=!3m1!4b1!4m5!3m4!1s0x3bae1670c9b44e6d:0xf8dfc3e8517e4fe0!8m2!3d12.9715987!4d77.5945627"><i class="fa fa-home fact-icon"></i>Bangalore, India</a>',
+    LinkedIn: `<a href="https://www.linkedin.com/in/ravindra-patil-08791720">
+                  <i class="fab fa-linkedin fact-icon"></i>Ravindra_Patil
                 </a>`,
-    'Twitter': `<a href="https://twitter.com/shakespeare"><i class="fa-brands fa-twitter fact-icon"></i>Shakespeare</a>`,
-    'Email': '<a href="mailto:wil64@yahoo.com"><i class="fa fa-envelope fact-icon"></i>wil64@yahoo.com</a>'
+    Twitter: `<a href="hhttps://twitter.com/PatilRavi035"><i class="fa-brands fa-twitter fact-icon"></i>Ravindra_Patil</a>`,
+    Email:
+      '<a href="mailto:patil.ravi035@gmail.com"><i class="fa fa-envelope fact-icon"></i>patil.ravi035@gmail.com</a>',
   },
   skills: [
-    ['Comedy', 100],
-    ['Love', 100],
-    ['Theater', 100],
-    ['Tragedy', 100],
-    ['Poetry', 100],
-    ['Erotic', 75],
-    ['Sonnets', 100],
-    ['Plays', 100],
-    ['Writing', 100],
-    ['Directing', 75],
-    ['Literature', 100],
-    ['Influence', 100],
+    ["React JS", 100],
+    ["Next JS", 75],
+    ["Typescript", 75],
+    ["Javascript", 100],
+    ["HTML 5", 100],
+    ["Ionic 4", 100],
+    ["Git", 75],
+    ["Angular 8", 75],
+    ["RXJS", 100],
+    ["CSS3", 100],
+    ["Material UI", 75],
+    ["SCSS", 75],
   ],
   positions: [
     {
-      title: 'Hamlet',
-      period: 'Likely early 17th century',
-      skills: ['tragedy', 'philosophy', 'death'],
-      contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
-`
+      title: "Philips - DLS (Design Language System)",
+      subTitle: "Senior Software Engineer",
+      period: "Nov 2021 - Present",
+      skills: [
+        "React JS",
+        "Typescript",
+        "Next JS",
+        "HTML 5",
+        "SCSS",
+        "Javascript",
+      ],
+      contents: ``,
     },
     {
-      title: 'Twelfth Night',
-      period: '1600 – 1601',
-      skills: ['comedy', 'love'],
-      contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-`
+      title: "TCS - Multiple Projects",
+      subTitle: "Senior Software Engineer",
+      period: "Nov 2014 – Nov 2021",
+      skills: [
+        "React JS",
+        "Typescript",
+        "Angular 6/7/8",
+        "RXJS",
+        "Ionic 4",
+        "Cordova",
+        "HTML 5",
+        "CSS 3",
+        "SCSS",
+        "Material UI",
+        "Web pack",
+      ],
+      contents: ` 
+      1. Worked on multiple web and hybrid mobile application development using various latest technologies 
+         like Ionic 4,Angular, React JS, Typescript, HTML 5, Javascript and CSS3.
+      2. Worked closely with business analysts, development teams and infrastructure specialists to deliver 
+         high availability solutions for mission critical applications.
+      3. Trained and mentored junior developers and Engineers, teaching skills like React JS and Angular.
+      4. Created proofs of concept for innovative new solutions.
+      5. Wrote quality code to program complete applications.
+      6. Have Experience working in client locations and worked in KLM(2.6 years), Amsterdam, Netherlands.
+      `,
     },
     {
-      title: 'The Tragedy of Romeo and Juliet',
-      period: '1595–1596, with a possible early draft written in 1591',
-      skills: ['tragedy', 'death', 'love'],
+      title: "Starmark Software Pvt. Ltd",
+      subTitle: "Senior Software Engineer",
+      period: "July 2013 - Nov 2014",
+      skills: [
+        "HTML 5",
+        "Javascript",
+        "Angular",
+        "Cordova",
+        "Telerik app builder tool",
+      ],
       contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-`
-    }
+      1. Developed Health Care Product using programming languages like Angular, Javascript, Cordova using 
+         Telerik app builder tool.
+      2. Application developed for Android and iOS platforms.
+      3. Designed frontend architecture for the application.
+      4. Deployed application to Apple store and Android play store multiple times.
+      5. Got appreciation for on time completion of the application and
+         for the quality coding.
+      `,
+    },
+    {
+      title: "Radix Web",
+      subTitle: "Software Engineer",
+      period: "April 2011 - June 2013",
+      skills: ["HTML 5", "Javascript", "Angular", "CSS", "Jquery"],
+      contents: `
+      1. Worked on multiple Web and Hybrid mobile application development using Cordova/Phonegap, HTML5, CSS, Jquery
+         Mobile and Javascript technologies.
+      2. Created executables binaries files(ipa and apk) using Cordova/Phonegap for multiple mobile platforms.
+      3. Submitting Android and iOS executable files to Apple Store and
+         Google Play store.
+      4. Deployed application to Apple store and Android play store multiple times.
+      5. Got appreciation for on time completion of the application and
+         for the quality coding.
+      `,
+    },
   ],
   experience: [
     {
-      header: 'Performance',
-      skills: ['theater', 'play'],
-      contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
-`
+      header: "LANGUAGES",
+      skills: ["English", "Kannada", "Hindi"],
+      contents: ``,
     },
     {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
-      contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. I dedicated them to Henry Wriothesley, Earl of Southampton.
-`
-    }
-  ]
+      header: "AWARDS",
+      skills: [
+        "2020/2021 - Credit Suisse - On Spot award for Delivery of project",
+        "2018-2019- KLM Royal Dutch Airlines - On spot award for developing and delivering most complex features",
+        "2014- Best developer Award for Developing Quality product and on time delivery Multiple appreciations for quick learning new technologies and for implementations",
+      ],
+      contents: ``,
+    },
+  ],
 };
